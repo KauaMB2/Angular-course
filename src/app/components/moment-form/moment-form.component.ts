bf06784001fe32411cdfaa6db87a65d286431c13
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-01-08 17:08:14 -0300

    Aula28

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
   @Input() btnText!:string
+  momentForm!: FormGroup
+
+  ngOnInit():void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+  }
+
+  get title(){//Get the title of the inserted in the form
+    return this.momentForm.get('title')!
+  }
+
+  get description(){//Get the description inserted in the form
+    return this.momentForm.get('description')!
+  }
+
+  submit():void{
+    console.log("Enviou!")
+  }
 }
